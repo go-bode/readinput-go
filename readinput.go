4adc9c81717f2a2170d69341a49081ba2aef4155
felipe.martins <felipewmartins@gmail.com>
2016-11-30 22:37:12 -0300

    Spme features

--- readinput.go
+++ readinput.go
@@ -11,17 +11,23 @@ func main() {
 }
 
 func readInput() {
-	var input int
+	var input string
+	fmt.Println("Press a option above")
+	fmt.Println("h - show hostname")
+	fmt.Println("e - exit program")
+	fmt.Println("t - show user temp dir")
 	n, err := fmt.Scanln(&input)
 	if n < 1 || err != nil {
 		fmt.Println("Invalid input")
 	}
 
 	switch input {
-	case 1:
-		fmt.Println("hi")
-	case 2:
+	case "h":
+		fmt.Println(os.Hostname())
+	case "e":
 		os.Exit(2)
+	case "t":
+		fmt.Println(os.TempDir())
 	default:
 		fmt.Println("padrão")
 	}
