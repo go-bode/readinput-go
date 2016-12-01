5e93ef7e066b58117cd67903856eca6b00a398ca
felipe.martins <felipewmartins@gmail.com>
2016-11-30 22:17:21 -0300

    New tests

--- readinput.go
+++ readinput.go
@@ -1,9 +1,28 @@
 package main
 
-import (
-	"fmt"
-	)
+import "fmt"
+import "os"
 
-func main(){
-	fmt.Println("Digite uma tecla...\n")
+func main() {
+	fmt.Println("Press some key...")
+	for {
+		readInput()
+	}
+}
+
+func readInput() {
+	var input int
+	n, err := fmt.Scanln(&input)
+	if n < 1 || err != nil {
+		fmt.Println("Invalid input")
+	}
+
+	switch input {
+	case 1:
+		fmt.Println("hi")
+	case 2:
+		os.Exit(2)
+	default:
+		fmt.Println("padrão")
+	}
 }
